2370b9f1a7169131496fbe8a75f7ccf9f29837dc
sadman_ichha <sadmanichha1@gmail.com>
2022-12-13 16:56:20 +0600

    onboarding

--- lib/ui/views/onboarding_screen.dart
+++ lib/ui/views/onboarding_screen.dart
@@ -6,7 +6,13 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(),
+          ),
+        ],
+      ),
     );
   }
 }
