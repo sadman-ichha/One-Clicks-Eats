20c2941f65f14cfa6da12110b559143f4ead1e33
sadman_ichha <sadmanichha1@gmail.com>
2022-12-13 16:54:51 +0600

    onboarding

--- lib/ui/route/route.dart
+++ lib/ui/route/route.dart
@@ -1,12 +1,17 @@
 import 'package:get/get.dart';
+import 'package:one_clicks_eats/ui/views/onboarding_screen.dart';
 import 'package:one_clicks_eats/ui/views/splash_screen.dart';
 
 const String splash = "/splash-screen";
-//const String splash = "/splash-screen";
+const String onboarding = "/onboarding-screen";
 
 List<GetPage> getPages = [
   GetPage(
     name: splash,
     page: () => const SplashScreen(),
+  ),
+  GetPage(
+    name: onboarding,
+    page: () => const OnboardingScreen(),
   ),
 ];
