49d959f0b3d86c0cc9a1293bd3a2bf347ec71df8
sadman_ichha <sadmanichha1@gmail.com>
2022-12-13 15:52:08 +0600

    splash

--- lib/ui/views/splash_screen.dart
+++ lib/ui/views/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:one_clicks_eats/const/app_imags.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -7,6 +8,12 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Future.delayed(const Duration(seconds: 3), () => Get.toNamed(splash));
-    return Scaffold(body: Center(child: Image.asset(AppImages.splashLogo)));
+    return Scaffold(
+        body: Center(
+            child: Image.asset(
+      AppImages.splashLogo,
+      height: 108.0.h,
+      width: 118.0.w,
+    )));
   }
 }
