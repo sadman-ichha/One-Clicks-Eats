de17c8c8224577abb563572ceac145d7b7a08585
sadman_ichha <sadmanichha1@gmail.com>
2022-12-14 00:04:32 +0600

    signup

--- lib/ui/views/sign_up.dart
+++ lib/ui/views/sign_up.dart
@@ -8,17 +8,20 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-       crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Image(
-              height: 94.0.h,
-              width: 86.0.w,
-              image: AssetImage(AppImages.splashLogo),
+      body: SafeArea(
+        child: Column(
+         crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 40.0.h),
+            Center(
+              child: Image(
+                height: 94.0.h,
+                width: 86.0.w,
+                image: AssetImage(AppImages.splashLogo),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
