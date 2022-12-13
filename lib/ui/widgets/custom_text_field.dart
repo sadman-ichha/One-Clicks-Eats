625df675bb5bced479d42920dbded50be30aeba5
sadman_ichha <sadmanichha1@gmail.com>
2022-12-14 00:20:11 +0600

    widget

---
+++ lib/ui/widgets/custom_text_field.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 57.0.h,
+        width: 307.0.w,
+        child: TextFormField(
+          decoration: InputDecoration(border: OutlineInputBorder()),
+        ));
+  }
+}
