73a0a1814010d8f5bba58d692ccfc55f067538b2
sadman_ichha <sadmanichha1@gmail.com>
2022-12-13 17:48:14 +0600

    onbroading

--- lib/ui/views/onboarding_screen.dart
+++ lib/ui/views/onboarding_screen.dart
@@ -10,7 +10,7 @@ class OnboardingScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.only(left: 35.0.w, right: 35.0.w),
-        child: Column(
+        child: Column( 
           children: [
             Expanded(
               flex: 1,
@@ -19,14 +19,16 @@ class OnboardingScreen extends StatelessWidget {
             Expanded(
               flex: 1,
               child: Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     "We have Quality Chief",
                     style: AppStyles.myTitleTextStyle,
+                    textAlign: TextAlign.start,
                   ),
-                  SizedBox(height: 20.0.h),
+                  SizedBox(height: 14.0.h),
                   Text(
-                    "ust few click to enter our foodis online app to get best service and We maintained quality.",
+                    "Just few click to enter our foodis online app to get best service and We maintained quality.",
                     style: AppStyles.mySubTitleTextStyle,
                   )
                 ],
