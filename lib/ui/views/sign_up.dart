86455dc56cbd90797d63c0c2f3b6ac31bd014e99
sadman_ichha <sadmanichha1@gmail.com>
2022-12-13 21:47:40 +0600

    signUp

--- lib/ui/views/sign_up.dart
+++ lib/ui/views/sign_up.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:one_clicks_eats/const/app_imags.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Image(
+            image: AssetImage(AppImages.splashLogo),
+          ),
+        ],
+      ),
+    );
   }
 }
