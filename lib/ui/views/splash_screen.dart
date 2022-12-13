0204e1b40df166191c689ab6f321a617d76f1bf5
sadman_ichha <sadmanichha1@gmail.com>
2022-12-13 16:06:45 +0600

    splash

--- lib/ui/views/splash_screen.dart
+++ lib/ui/views/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:one_clicks_eats/const/app_colors.dart';
 import 'package:one_clicks_eats/const/app_imags.dart';
 
 class SplashScreen extends StatelessWidget {
