b0e96759c4d855abe3f4d263eeda1fe4bff7348b
sadman_ichha <sadmanichha1@gmail.com>
2022-12-14 13:40:05 +0600

    login

--- lib/ui/styles/app_style.dart
+++ lib/ui/styles/app_style.dart
@@ -1,17 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get_core/src/get_main.dart';
-
+import 'package:get/get.dart';
+import 'package:one_clicks_eats/ui/routes/route.dart';
 import '../../const/app_colors.dart';
 
 class AppStyles {
-  static TextStyle myTitleTextStyle = TextStyle(
-      fontSize: 27.0.sp, fontWeight: FontWeight.w600, color: Color(0xFF343B42));
-  static TextStyle mySubTitleTextStyle = TextStyle(
-      fontSize: 16.0.sp, fontWeight: FontWeight.w400, color: Color(0xFF8D8D8D));
-
-      RichText richText(String firstText, String secText, navigatePage) => RichText(
+  RichText richText(String firstText, String secText, navigatePage) => RichText(
         text: TextSpan(
             text: firstText,
             style: TextStyle(
@@ -27,8 +22,13 @@ class AppStyles {
                     fontWeight: FontWeight.w400,
                     color: AppColors.appleColor),
                 recognizer: TapGestureRecognizer()
-                  ..onTap = () => Get.toNamed(),
+                  ..onTap = () => Get.toNamed(login),
               )
             ]),
       );
+
+  static TextStyle myTitleTextStyle = TextStyle(
+      fontSize: 27.0.sp, fontWeight: FontWeight.w600, color: Color(0xFF343B42));
+  static TextStyle mySubTitleTextStyle = TextStyle(
+      fontSize: 16.0.sp, fontWeight: FontWeight.w400, color: Color(0xFF8D8D8D));
 }
