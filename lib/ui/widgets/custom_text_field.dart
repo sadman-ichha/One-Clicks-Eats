e8cce7b45a1733fd2af4bec361e8f89b79b2c35b
sadman_ichha <sadmanichha1@gmail.com>
2022-12-14 12:06:23 +0600

    custom button

--- lib/ui/widgets/custom_text_field.dart
+++ lib/ui/widgets/custom_text_field.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  //const CustomTextField({super.key});
+
+  String? hintsText;
+  CustomTextField(this.hintsText);
 
   @override
   Widget build(BuildContext context) {
