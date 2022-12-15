64d4db26fb628052b068a2acd6d9334465847386
sadman_ichha <sadmanichha1@gmail.com>
2022-12-15 13:13:31 +0600

    signumodel

--- lib/helper/signup_helper.dart
+++ lib/helper/signup_helper.dart
@@ -1,3 +1,26 @@
+import 'package:one_clicks_eats/const/server_config.dart';
+import 'package:one_clicks_eats/models/sign_up_model.dart';
+import 'package:http/http.dart' as http;
+
 class SignUpHelper {
- Future 
+  Future<SignUpModel?> signupRegister(String name, dynamic emailOrPhone,
+      String passWord, String confirmPassWord, registerBy) async {
+    String url = Config.serverURl + Config.signupURl;
+    var body = {
+      "name": name,
+      "email_or_phone": emailOrPhone,
+      "password": passWord,
+      "password_confirmation": confirmPassWord,
+      "register_by": registerBy,
+    };
+
+    var response = await http.post(Uri.parse(url), body: body);
+    if (response.statusCode == 201) {
+      var json = response.body;
+      var signUpModel = signUpModelFromJson(json);
+      if (signUpModel.result== false){
+        
+      }
+    }
+  }
 }
