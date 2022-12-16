6da2fd61a0fcfc0b75954e0ca1f6039cdd321bdf
sadman_ichha <sadmanichha1@gmail.com>
2022-12-16 17:35:55 +0600

    otp

--- lib/helper/otp_helper.dart
+++ lib/helper/otp_helper.dart
@@ -17,7 +17,7 @@ class OTPHelper {
     if (response.statusCode == 200) {
       otpModel = otpModelFromJson(response.body);
       Fluttertoast.showToast(msg: otpModel.message.toString());
-    }else {
+      print("Success __${response.body}");
       
     }
     return null;
