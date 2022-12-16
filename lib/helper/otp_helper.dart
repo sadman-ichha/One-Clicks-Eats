e9c0ec2fd3245085ac6af76a66559fb81396e600
sadman_ichha <sadmanichha1@gmail.com>
2022-12-16 17:23:42 +0600

    otp

--- lib/helper/otp_helper.dart
+++ lib/helper/otp_helper.dart
@@ -6,7 +6,7 @@ import 'package:one_clicks_eats/models/otp_model.dart';
 class OTPHelper {
   Future<OtpModel?> otpVerification(userID, verificationCode) async {
     String url = Config.serverURl + Config.confirmCodeURL;
-    Map body = {"user_id": userID, "verification_code": verificationCode};
+    Map body = {"user_id": userID, "verification_code": verificationCode,};
     http.Response response = await http.post(Uri.parse(url), body: body);
     return null;
   }
