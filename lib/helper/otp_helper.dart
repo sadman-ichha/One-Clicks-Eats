76b586c14c2662cf2802cb09a2821cc6c86cfc22
sadman_ichha <sadmanichha1@gmail.com>
2022-12-16 17:39:58 +0600

    otp

--- lib/helper/otp_helper.dart
+++ lib/helper/otp_helper.dart
@@ -5,22 +5,26 @@ import 'package:one_clicks_eats/models/otp_model.dart';
 
 class OTPHelper {
   Future<OtpModel?> otpVerification(userID, verificationCode) async {
-    String url = Config.serverURl + Config.confirmCodeURL;
-    var headers = {"Accept": "application/json"};
-    OtpModel? otpModel;
-    Map body = {
-      "user_id": userID,
-      "verification_code": verificationCode,
-    };
-    http.Response response =
-        await http.post(Uri.parse(url), headers: headers, body: body);
-    if (response.statusCode == 200) {
-      otpModel = otpModelFromJson(response.body);
-      Fluttertoast.showToast(msg: otpModel.message.toString());
-      print("Success __${response.body}");
-    } else {
-      throw Fluttertoast.showToast(msg: otpModel!.message.toString());
+    try {
+      String url = Config.serverURl + Config.confirmCodeURL;
+      var headers = {"Accept": "application/json"};
+      OtpModel? otpModel;
+      Map body = {
+        "user_id": userID,
+        "verification_code": verificationCode,
+      };
+      http.Response response =
+          await http.post(Uri.parse(url), headers: headers, body: body);
+      if (response.statusCode == 200) {
+        otpModel = otpModelFromJson(response.body);
+        Fluttertoast.showToast(msg: otpModel.message.toString());
+        print("Success __${response.body}");
+      } else {
+        throw Fluttertoast.showToast(msg: otpModel!.message.toString());
+      }
+      return otpModel;
+    } catch (e) {
+      throw Fluttertoast.showToast(msg: "Something went Wrong");
     }
-    return otpModel;
   }
 }
