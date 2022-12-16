bdd190341fb0783f64cf50d72cedae7fb478ff3a
sadman_ichha <sadmanichha1@gmail.com>
2022-12-16 17:18:53 +0600

    otp

--- lib/const/server_config.dart
+++ lib/const/server_config.dart
@@ -1,4 +1,5 @@
 class Config {
   static const String serverURl = "https://bdraj.com/api/v2/auth/";
   static const String signupURl = "signup";
+  static const String confirmCodeURL = "confirm_code";
 }
--- lib/helper/otp_helper.dart
+++ lib/helper/otp_helper.dart
@@ -1,10 +1,11 @@
-import 'package:http/http.dart'as http;
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import 'package:one_clicks_eats/const/server_config.dart';
 import 'package:one_clicks_eats/models/otp_model.dart';
 
-class OTPHelper{
-  Future<OtpModel?> otpVerification()async{
-   
-   http.post(url)
-
+class OTPHelper {
+  Future<OtpModel?> otpVerification() async {
+    String url = Config.serverURl + Config.confirmCodeURL;
+    http.Response response =await http.post(Uri.parse(url));
   }
 }
