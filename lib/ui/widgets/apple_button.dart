7fa6b3860f31e453a68294e7879c893779869e95
sadman_ichha <sadmanichha1@gmail.com>
2022-12-22 14:18:59 +0600

    validate

--- lib/ui/widgets/apple_button.dart
+++ lib/ui/widgets/apple_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../const/app_colors.dart';
 
 class AppleButton extends StatelessWidget {
