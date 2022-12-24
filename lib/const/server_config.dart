babbe8558f801751fd14f4c828a91e67cd163ed4
sadman_ichha <sadmanichha1@gmail.com>
2022-12-25 00:24:54 +0600

    login

--- lib/const/server_config.dart
+++ lib/const/server_config.dart
@@ -2,4 +2,5 @@ class Config {
   static const String serverURl = "https://bdraj.com/api/v2/auth/";
   static const String signupURl = "signup";
   static const String confirmCodeURL = "confirm_code";
+  static const String loginURL = "login";
 }
--- lib/helper/login_helper.dart
+++ lib/helper/login_helper.dart
@@ -1,8 +1,15 @@
+import 'package:get/get.dart';
+import 'package:one_clicks_eats/const/server_config.dart';
 import 'package:one_clicks_eats/models/login_model.dart';
+import 'package:http/http.dart' as http;
 
 class LogInHelper {
   Future<LoginModel?> login(dynamic emailORPhone, String password) async {
-    
-
+    String url = Config.serverURl + Config.loginURL;
+    Map body = {
+      "email": emailORPhone,
+      "password": password,
+    };
+    http.Response response = await http.post(Uri.parse(url), body: body);
   }
 }
