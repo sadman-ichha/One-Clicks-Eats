f1b943c1e261f570db4e594c7a3c4293f45e2a4d
sadman_ichha <sadmanichha1@gmail.com>
2022-12-31 11:46:40 +0600

    bottom nav

--- lib/ui/views/bottom_nav_controller.dart/pages/home_screen.dart
+++ lib/ui/views/bottom_nav_controller.dart/pages/home_screen.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get/get.dart';
-import 'package:one_clicks_eats/const/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   // const HomeScreen({super.key});
-
-  RxInt _currentIndex = 0.obs;
 
   @override
   Widget build(BuildContext context) {
@@ -18,34 +14,6 @@ class HomeScreen extends StatelessWidget {
             child: FaIcon(FontAwesomeIcons.house),
           ),
         ],
-      ),
-      bottomNavigationBar: Obx(
-        () => BottomNavigationBar(
-            onTap: (val) {
-              _currentIndex.value = val;
-            },
-            currentIndex: _currentIndex.value,
-            elevation: 5.0,
-            selectedItemColor: AppColors.appleColor,
-            backgroundColor: Colors.black,
-            items: [
-              BottomNavigationBarItem(
-                label: "Home",
-                icon: FaIcon(FontAwesomeIcons.house, size: 15.0),
-              ),
-              BottomNavigationBarItem(
-                label: "Favourite",
-                icon: FaIcon(FontAwesomeIcons.heart),
-              ),
-              BottomNavigationBarItem(
-                label: "Cart",
-                icon: FaIcon(FontAwesomeIcons.bucket, size: 15.0),
-              ),
-              BottomNavigationBarItem(
-                label: "Profile",
-                icon: FaIcon(FontAwesomeIcons.house, size: 15.0),
-              ),
-            ]),
       ),
     );
   }
